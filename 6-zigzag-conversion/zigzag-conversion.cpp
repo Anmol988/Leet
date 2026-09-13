f87a012c338b2553a02dfb46dc9bcc90530b79d1
Anmol <anmolanmol9888@gmail.com>
2026-09-13 22:44:08 +0530

    Time: 4 ms (61.89%) | Memory: 14.2 MB (24.15%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        
+        vector<string> rows(numRows);
+        
+        int currentRow = 0;
+        int direction = 1;
+        
+        for (char c : s) {
+            rows[currentRow] += c;
+            
+            // Change direction at top and bottom
+            if (currentRow == 0) {
+                direction = 1;
+            }
+            else if (currentRow == numRows - 1) {
+                direction = -1;
+            }
+            
+            currentRow += direction;
+        }
+        
+        string answer = "";
+        
+        for (string row : rows) {
+            answer += row;
+        }
+        
+        return answer;
+    }
+};
